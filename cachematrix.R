465b1ea5a15b61d0d7cd95a8555b9c856182e236
Vladimir Lifliand <vlflnd@gmail.com>
2015-01-21 22:24:05 -0800

    complete the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## Creates a closure containing the matrix data and optionally
+## the inverted matrix. The semantics of the inverted matrix is
+## one of a cache. Inverted matrix is invalidated when source
+## matrix is changed.
 makeCacheMatrix <- function(x = matrix()) {
-
+    s <- NULL
+    set <- function(y) {
+        x <<- y
+        s <<- NULL
+    }
+    get <- function() x
+    setsolved <- function(solved) s <<- solved
+    getsolved <- function() s
+    list(set = set,
+         get = get,
+         setsolved = setsolved,
+         getsolved = getsolved)
 }
 
-
-## Write a short comment describing this function
-
+## Computes the inverse of a matrix closure created by 
+## makeCacheMatrix. The solved matrix is cached, so that subsequent
+## calls return the cached matrix rather than repeating the 
+## calculation.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    s <- x$getsolved()
+    if(!is.null(s)) {
+        message("getting cached data")
+        return(s)
+    }
+    data <- x$get()
+    s <- solve(data, ...)
+    x$setsolved(s)
+    s
 }
